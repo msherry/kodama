c21e7b9edb7e7906ec2575351f25eee6bc260745
Marc Sherry <marc@imo.im>
2010-11-04 16:47:55 -0700

    Command-line parsing

--- kodama.c
+++ kodama.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "kodama.h"
 
+struct globals {
+    int i;
+} globals;
+
+void usage(char *arg0)
+{
+    fprintf(stderr, "Usage: %s [options]...\n", arg0);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "-h: this help\n");
+}
+
+void parse_command_line(int argc, char *argv[])
+{
+  int c;
+
+  globals.i = 0;
+  opterr = 0;
+  while ((c = getopt(argc, argv, "h")) != -1)
+  {
+    switch (c)
+    {
+    case 'h':
+        usage(argv[0]);
+        exit(0);
+        break;
+    /* case 'i': */
+    /*   globals.in_file = g_strdup_printf("%s", optarg); */
+    /*   break; */
+    case '?':
+      fprintf(stderr, "Unknown option %c.\n", optopt);
+      exit(1);
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
-  UNUSED(argc);
-  UNUSED(argv);
+  parse_command_line(argc, argv);
 
   return 0;
 }
